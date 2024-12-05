7c6838609be431b9477e78030bbebacab91e5aaa
Tejaswy Yamarthi <tejaswy07@gmail.com>
2024-12-05 15:21:36 +0000

    Variable's and User Input Examples

---
+++ variables.py
@@ -0,0 +1,37 @@
+
+#Variable examples
+
+
+First_Name="Pratice Makes"
+Last_Name="Man's Perfect"
+
+print(First_Name+Last_Name)
+
+
+#2.check what kind of Datatype used by using type 
+
+First_Name="Hello Girl"
+
+print(type(First_Name))
+
+
+#3.concatination can be don when the datatypes are only equal
+
+
+First_name="Robert Rank"
+Rank=("1")
+
+print(First_name+Rank)
+
+
+'''Throws error if we try to join different types of data types 
+First_name="Robert Rank" ====str
+Rank=1==int
+
+print(First_name+Rank)'''
+
+
+#4.user Input
+
+Give_Name=input("Enter your Name : ")
+print("Your Name is" +" " + Give_Name + " "  + "is that Correct ?" )
