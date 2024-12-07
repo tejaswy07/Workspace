5f9e51ff9e9843cdddb306de0bfecd0f0cda1d3b
Tejaswy Yamarthi <tejaswy07@gmail.com>
2024-12-07 21:12:32 +0000

    Need to double check the File

--- if,elif,else.py
+++ if,elif,else.py
@@ -24,7 +24,7 @@ else:
 
 #2.Write a program that takes two numbers as input and prints the larger number.
 
-Number1=int(input("Enter the First Number : "))
+'''Number1=int(input("Enter the First Number : "))
 Number2=int(input("Enter the Second Number : "))
 
 if Number1>Number2:
@@ -32,9 +32,28 @@ if Number1>Number2:
 elif Number2>Number1:
     print(f"{Number2} is the largest number")
 else:
-    print(f"end")
+    print(f"end")'''
+    
+    
+#3.Write a program that takes a character as input and prints "Vowel" if it's a vowel (a, e, i, o, u), and "Consonant" otherwise.
 
- 
+Name=input("Enter the Name : ")
+vowel=["a","e","i","o","u"]
+
+
+contains_vowel = any(char in vowel for char in Name) 
+contains_consonant = any(char.isalpha() and char not in vowel for char in Name)
+
+print(contains_vowel)
+print(contains_consonant)
+
+'''if Name.find("a","e","i","o","u"):
+    print("Given name has Vowel words ")
+elif Name!=Name.find("a","e","i","o","u"):
+    print("Given name has Consonents words only")
+else:
+    print("end")'''      
+
  
     
     
