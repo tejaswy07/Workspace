18919e09ac081f8106d58d378450a248dbdd79b3
Tejaswy Yamarthi <tejaswy07@gmail.com>
2024-12-07 20:50:26 +0000

    Tested with basic examples

--- Test.py
+++ Test.py
@@ -2,4 +2,13 @@
 
 
 name="teja"
-print("Hello"+name)
+#print("Hello"+name)
+
+print(name.capitalize()) #--only first letter is capitalized
+print(name.find("a"))
+print(name.upper()) #--ALL CAPITALS
+print(name.lower()) #--All lower
+print(name.isdigit()) #--checks is it a number or not
+print(name.isalpha()) #Checks it consists of only string letters then the output gives True but if consists of spaces,Numbers,special characters the it returns False                
+print(name.count("e"))    
+print(name.replace("t","a"))                
