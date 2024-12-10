4f130146dc43af290d9468b9f31e0b657549e9d1
Tejaswy Yamarthi <tejaswy07@gmail.com>
2024-12-10 21:50:40 +0000

    For Loop Examples

---
+++ For.py
@@ -0,0 +1,36 @@
+#for item in sequence:
+    # Code block to be executed for each item in the sequence
+
+
+# Example: Printing numbers from 1 to 5 using a for loop
+
+for num in range(1, 6):
+    print(num)
+    
+    
+ #Use a for loop to calculate the sum of numbers from 1 to 10.
+
+sum=0 
+ 
+for i in range(0,10):
+    sum +=i
+print(sum)    
+
+#Write a for loop to print each character in the string "Hello".
+
+h="hello"
+
+for j in h:
+    print(j)
+
+#Write a for loop to print the first 10 even numbers.
+
+for even in range(0,20):
+    if even % 2 == 0:
+        print(even)
+        
+#Create a for loop that doubles each number in the sequence: 1, 2, 3, 4, 5.
+
+for x in range(0,10,2):
+    print(x)        
+       
