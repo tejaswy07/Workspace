3500274e9ef0f6256d2acac8d7f394ac85eb0087
Tejaswy Yamarthi <tejaswy07@gmail.com>
2024-12-28 20:35:46 +0000

    pythonFile

---
+++ python.py
@@ -0,0 +1,22 @@
+for num in range(1, 6):
+    pass
+              
+              
+for num in range(1, 6):
+    if num % 2 == 0:
+        continue
+    print(num)              
+    
+   
+
+for x in range(1,6):
+    if x == 3:
+     continue
+    print(x)    
+    
+check="hello world"
+
+for x in check:
+    if x=="o":  
+      break
+    print(x)
